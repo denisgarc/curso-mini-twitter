f66d9534bce054a8186e271c2bd63793f169c765
Dennis Oxlaj <denisgarc@hotmail.com>
2022-04-20 21:42:15 -0600

    S08-CL07 y CL08: Se agrega activity para sing up y se mejora estilo.

--- app/src/main/java/com/androidavanzado/minitwitter/MainActivity.java
+++ app/src/main/java/com/androidavanzado/minitwitter/MainActivity.java
@@ -2,13 +2,47 @@ package com.androidavanzado.minitwitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLogin;
+    TextView textviewGoSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        btnLogin = findViewById(R.id.buttonLogin);
+        textviewGoSignUp = findViewById(R.id.textViewGoSignUp);
+
+        btnLogin.setOnClickListener(this);
+        textviewGoSignUp.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch (id){
+            case R.id.buttonLogin:
+                break;
+            case R.id.textViewGoSignUp:
+                goToSignUp();
+                break;
+        }
+    }
+
+    private void goToSignUp() {
+        Intent i = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(i);
     }
 }
---
+++ app/src/main/java/com/androidavanzado/minitwitter/SignUpActivity.java
@@ -0,0 +1,48 @@
+package com.androidavanzado.minitwitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView textViewBackLogin;
+    Button buttonSignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        getSupportActionBar().hide();
+
+        textViewBackLogin = findViewById(R.id.textViewBackLogin);
+        buttonSignUp = findViewById(R.id.buttonSignUp);
+
+        textViewBackLogin.setOnClickListener(this);
+        buttonSignUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch (id){
+            case R.id.buttonSignUp:
+                break;
+            case R.id.textViewBackLogin:
+                goToLogin();
+                break;
+        }
+    }
+
+    private void goToLogin() {
+        Intent i = new Intent(SignUpActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
