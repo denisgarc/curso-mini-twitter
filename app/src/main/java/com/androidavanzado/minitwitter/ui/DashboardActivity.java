8b2dea95aba3685b976a447204d128c75f112391
Dennis Oxlaj <denisgarc@hotmail.com>
2022-04-22 03:15:09 -0600

    feat: Se realizan ajustes para que funciona el navbar

--- app/src/main/java/com/androidavanzado/minitwitter/ui/DashboardActivity.java
+++ app/src/main/java/com/androidavanzado/minitwitter/ui/DashboardActivity.java
@@ -11,7 +11,10 @@ import com.google.android.material.navigation.NavigationBarView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 public class DashboardActivity extends AppCompatActivity implements NavigationBarView.OnItemReselectedListener {
 
@@ -27,15 +30,15 @@ public class DashboardActivity extends AppCompatActivity implements NavigationBa
         getSupportActionBar().hide();
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        navView.setOnItemReselectedListener(this);
+        // navView.setOnItemReselectedListener(this);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_tweets_like, R.id.navigation_profile)
                 .build();
-        /*NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_dashboard);
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_dashboard);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);*/
+        NavigationUI.setupWithNavController(navView, navController);
 
     }
 
