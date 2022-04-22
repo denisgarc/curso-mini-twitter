20750b27c6034c70be9a49bf34e1c3f1841d7c3b
Dennis Oxlaj <denisgarc@hotmail.com>
2022-04-22 01:43:21 -0600

    S08-CL16 Y CL17: Personalizacion de pantalla

--- app/src/main/java/com/androidavanzado/minitwitter/ui/DashboardActivity.java
+++ app/src/main/java/com/androidavanzado/minitwitter/ui/DashboardActivity.java
@@ -1,21 +1,19 @@
 package com.androidavanzado.minitwitter.ui;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import com.androidavanzado.minitwitter.R;
-import com.androidavanzado.minitwitter.common.Constantes;
-import com.androidavanzado.minitwitter.common.SharedPreferenceManager;
 import com.androidavanzado.minitwitter.databinding.ActivityDashboardBinding;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-public class DashboardActivity extends AppCompatActivity {
+public class DashboardActivity extends AppCompatActivity implements NavigationBarView.OnItemReselectedListener {
 
     private ActivityDashboardBinding binding;
 
@@ -23,19 +21,34 @@ public class DashboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        binding = ActivityDashboardBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        // binding = ActivityDashboardBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_dashboard);
+
+        getSupportActionBar().hide();
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
+        navView.setOnItemReselectedListener(this);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                R.id.navigation_home, R.id.navigation_tweets_like, R.id.navigation_profile)
                 .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_dashboard);
+        /*NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_dashboard);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(binding.navView, navController);
+        NavigationUI.setupWithNavController(binding.navView, navController);*/
 
     }
 
+    @Override
+    public void onNavigationItemReselected(@NonNull MenuItem item) {
+        Toast.makeText(this, "entra click navigation", Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()){
+            case R.id.navigation_home:
+                break;
+            case R.id.navigation_tweets_like:
+                break;
+            case R.id.navigation_profile:
+                break;
+        }
+    }
 }
